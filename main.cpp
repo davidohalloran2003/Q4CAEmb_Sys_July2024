7ac7a147351fc48eefaed51bc8ad95163a3e8046
davidohalloran2003 <davidohalloran2003@gmail.com>
2024-07-12 14:33:22 +0000

    Question4_CA_Em_Sys

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Initialise variables
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    // Add a and b, store the result in d
+    int d = a + b;
+    printf("Value of d (a + b): %d\n", d);
+
+    // Subtract c from d, store the result in e
+    int e = d - c;
+    printf("Value of e (d - c): %d\n", e);
+
+    return 0;
+}
